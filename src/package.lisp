d479e17fd291816046e2c4649b8951f734c48400
Michael Fiano <mail@michaelfiano.com>
2019-02-12 08:56:29 -0500

    More work on organizing exported symbols

--- src/package.lisp
+++ src/package.lisp
@@ -5,69 +5,82 @@
   (:use #:cl)
 
   ;; engine
-  (:export #:launch
-           #:stop)
+  (:export
+   #:launch
+   #:stop)
 
   ;; actions
-  (:export #:action
-           #:make-action
-           #:on-action-insert
-           #:on-action-update
-           #:on-action-finish
-           #:action-step
-           #:action/fade
-           #:action/sprite-animate
-           #:action/translate
-           #:action/rotate)
+  (:export
+   #:action
+   #:make-action
+   #:on-action-insert
+   #:on-action-update
+   #:on-action-finish
+   #:action-step
+   #:action/fade
+   #:action/sprite-animate
+   #:action/translate
+   #:action/rotate)
 
-  (:export #:cache-lookup
-           #:camera
-           #:define-asset-pool
-           #:define-component
-           #:define-framebuffer
-           #:define-material
-           #:define-options
-           #:define-prefab
-           #:define-resource-paths
-           #:define-scenes
-           #:define-shader
-           #:define-shader-function
-           #:define-shader-struct
-           #:define-texture
-           #:define-texture-profile
-           #:entity
-           #:game-state
-           #:get-entity-component
-           #:input-enter-p
-           #:input-enabled-p
-           #:input-exit-p
-           #:make-shader-blocks
-           #:make-shader-buffer
-           #:make-entity
-           #:make-scene
-           #:material
-           #:mesh
-           #:on-component-attach
-           #:on-component-create
-           #:on-component-destroy
-           #:on-component-detach
-           #:on-component-update
-           #:on-component-render
-           #:print-prefab
-           #:render
-           #:rotate-transform
-           #:scale-transform
-           #:shader-modified-post-hook
-           #:sprite
-           #:storage
-           #:switch-camera-target
-           #:switch-scene
-           #:tag
-           #:transform
-           #:translate-transform
-           #:uniforms
-           #:value
-           #:write-shader-buffer))
+  ;; assets
+  (:export
+   #:asset
+   #:define-asset-pool)
+
+  ;; prefabs
+  (:export
+   #:define-prefab
+   #:print-prefab)
+
+  ;; components
+  (:export
+   #:on-component-attach
+   #:on-component-create
+   #:on-component-destroy
+   #:on-component-detach
+   #:on-component-update
+   #:on-component-render)
+
+  ;; input
+  (:export
+   #:input-enter-p
+   #:input-enabled-p
+   #:input-exit-p)
+
+  (:export
+   #:cache-lookup
+   #:camera
+   #:define-component
+   #:define-framebuffer
+   #:define-material
+   #:define-options
+   #:define-resource-paths
+   #:define-scenes
+   #:define-texture
+   #:define-texture-profile
+   #:entity
+   #:game-state
+   #:get-entity-component
+   #:make-shader-blocks
+   #:make-shader-buffer
+   #:make-entity
+   #:make-scene
+   #:material
+   #:mesh
+   #:render
+   #:rotate-transform
+   #:scale-transform
+   #:shader-modified-post-hook
+   #:sprite
+   #:storage
+   #:switch-camera-target
+   #:switch-scene
+   #:tag
+   #:transform
+   #:translate-transform
+   #:uniforms
+   #:value
+   #:write-shader-buffer))
 
 (defpackage+ #:bloom.shader
   (:local-nicknames (#:b #:bloom))
